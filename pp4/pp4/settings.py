7eba35e30f461f4919b3ce9f5a2222260ac77bff
Joseph Smith <jts272@outlook.com>
2023-05-09 18:11:45 +0100

    build: connect postgres database
    A database was provisioned from ElephantSQL, the URL of which is stored in the untracked env.py file.

--- pp4/pp4/settings.py
+++ pp4/pp4/settings.py
@@ -16,6 +16,9 @@ import os
 if os.path.isfile('env.py'):
     import env  # noqa
 
+# For parsing our provisioned Postgres database URL
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -41,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',
 ]
 
 MIDDLEWARE = [
@@ -77,11 +81,15 @@ WSGI_APPLICATION = 'pp4.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
 
 
