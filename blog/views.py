d15fd056f48d6a6ac63b0e836fa24bdb5739661a
Joseph Smith <jts272@outlook.com>
2023-05-10 18:46:26 +0100

    feat: setup view to display blog posts
    Django generic views were inherited by a class-based view to handle much of the functionality, such as pagination.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+# Utilize Django's generic class-based views
+from django.views import generic
+# Import the models that the views are based upon
+from .models import Post
 
 # Create your views here.
+
+
+class PostList(generic.ListView):
+    """A generic class-based view that inherits from Django's built-in
+    generic module
+
+    Reference: https://youtu.be/LP-glKOWpi8
+    """
+    # The model this class will use
+    model = Post
+    # Get published posts, newest first
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    # The HTML page that the view will render
+    template_name = 'blog.html'
+    # Set the maximum number of posts to appear on one page
+    # Navigation will be added automatically when the number is exceeded
+    paginate_by = 4
