1d1cfe397e03335e63ae7649a990cbf2e8871a8d
Joseph Smith <jts272@outlook.com>
2023-05-10 15:08:19 +0100

    feat: register comment model in admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 # Import the models to be accessed in the admin panel
-from .models import Post
+from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 
@@ -12,3 +12,22 @@ class PostAdmin(SummernoteModelAdmin):
     # Further information on use in Django admin:
     # https://github.com/summernote/django-summernote#django-admin-site
     summernote_fields = ('content')
+    # Map the slug field match that of the title
+    # Django handles the lowercasing and hyphenating via model SlugField
+    prepopulated_fields = {'slug': ('title',)}
+    # Additional QoL features implemented for admin panel used from:
+    # https://youtu.be/JGt1p8JhPyY
+    # The fields shown for each post in the list
+    list_display = ('title', 'slug', 'status', 'created_on')
+    # Search fields to find a post
+    search_fields = ('title', 'content')
+    # Filter posts by status and creation date
+    list_filter = ('status', 'created_on')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    # Customization of the Comment model fields in the admin panel
+    list_display = ('name', 'body', 'post', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ('name', 'email', 'body')
