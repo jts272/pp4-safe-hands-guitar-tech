147623ff03b9eb38e8f40c9d3017a7a1782476fc
Joseph Smith <jts272@outlook.com>
2023-05-13 13:51:29 +0100

    build: include localhost in allowed hosts
    The url path to localhost and its alias are read as two separate addresses in Django localhosts, so both versions were included for ease of access in local development.

--- pp4/settings.py
+++ pp4/settings.py
@@ -33,7 +33,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['pp4-safe-hands-guitar-tech.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = [
+    'pp4-safe-hands-guitar-tech.herokuapp.com',
+    '127.0.0.1',
+    'localhost']
 
 
 # Application definition
