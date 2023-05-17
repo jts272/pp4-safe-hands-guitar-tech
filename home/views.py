99d801c3859b5aa389be0d635f2e348715d41f27
Joseph <jts272@outlook.com>
2023-05-17 19:48:42 +0100

    refactor: use render shortcut for index page

--- home/views.py
+++ home/views.py
@@ -1,6 +1,4 @@
-from django.http import HttpResponse
 from django.shortcuts import render
-from django.template import loader
 
 from blog.models import Post
 
@@ -9,9 +7,5 @@ from blog.models import Post
 
 def index(request):
     latest_blog_posts = Post.objects.order_by('-created_on')[:3]
-    template = loader.get_template('home/index.html')
-    context = {
-        'latest_blog_posts': latest_blog_posts
-    }
-    # output = ', '.join([p.title for p in latest_blog_posts])
-    return HttpResponse(template.render(context, request))
+    context = {'latest_blog_posts': latest_blog_posts}
+    return render(request, 'home/index.html', context)
