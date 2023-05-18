8830b5d5b266f9eb5e9d015cfb9cc2ebf8034a23
Joseph <jts272@outlook.com>
2023-05-18 13:40:36 +0100

    feat: adjust service model admin panel view
    The SummernoteModelAdmin class was inherited to allow for WYSIWYG text editing.

--- services/admin.py
+++ services/admin.py
@@ -1,6 +1,33 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 
 from .models import Service
 
+
+class ServiceAdmin(SummernoteModelAdmin):
+    """This class defines the appearance and functionality of the
+    Service class in the admin panel."""
+    # Fields to display in list view
+    list_display = ('name', 'price')
+    # Group the layout of the admin view
+    fieldsets = (
+        (
+            'Service',
+            {
+                'fields': ('name', 'price', 'description')
+            }
+        ),
+        # Keep image-related information together
+        (
+            'Images',
+            {
+                'fields': ('featured_image', 'featured_image_alt')
+            }
+        ),
+    )
+    # Specify the fields that require the WSYIWYG editor
+    summernote_fields = ('description',)
+
+
 # Register your models here.
-admin.site.register(Service)
+admin.site.register(Service, ServiceAdmin)
