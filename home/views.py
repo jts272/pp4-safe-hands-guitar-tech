ded7df43fb28381929829660b90f273d50833580
Joseph <jts272@outlook.com>
2023-05-23 15:23:26 +0100

    feat: increase recent blog post home list to five
    The header was transformed into a link to the full blog post list view.

--- home/views.py
+++ home/views.py
@@ -6,6 +6,6 @@ from blog.models import Post
 
 
 def index(request):
-    latest_blog_posts = Post.objects.order_by('-created_on')[:3]
+    latest_blog_posts = Post.objects.order_by('-created_on')[:5]
     context = {'latest_blog_posts': latest_blog_posts}
     return render(request, 'home/index.html', context)
