3612942518d6e13485cc0d9d9d2ea50f17f7ff76
Joseph <jts272@outlook.com>
2023-05-25 12:08:02 +0100

    style: removed unused code from setups admin
    Code referencing unused models was still present in commented-out form. This has now been removed.

--- setups/admin.py
+++ setups/admin.py
@@ -1,33 +1,7 @@
 from django.contrib import admin
 
-from .models import Instrument, Job, Order, Product, Setup
+from .models import Job
 
-# class OrderInline(admin.TabularInline):
-#     model = Order
-
-
-# class ProductInline(admin.TabularInline):
-#     model = Product
-
-
-# class ProductAdmin(admin.ModelAdmin):
-#     inlines = [
-#         OrderInline
-#     ]
-
-
-# class OrderAdmin(admin.ModelAdmin):
-#     inlines = [
-#         ProductInline
-#     ]
-
-
-# # Register your models here.
-# admin.site.register(Setup)
-# admin.site.register(Instrument)
-
-# admin.site.register(Order, OrderAdmin)
-# admin.site.register(Product, ProductAdmin)
 
 class JobAdmin(admin.ModelAdmin):
     list_display = ('image', 'user', 'instrument', 'date_in', 'job_status')
