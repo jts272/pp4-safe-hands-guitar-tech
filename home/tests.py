60eb842e219ef5dfa6ec7560ce48ab855f06a62a
Joseph <jts272@outlook.com>
2023-05-27 15:42:53 +0100

    test: home app index view
    Testing is performed on the SQLite3 database that comes with the Django project. To enable switching between databases easily, the local and production databases were split off into their own variables.

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+
+class TestViews(TestCase):
+    """Tests for the home app, whose only view renders the homepage,
+    with recent blog posts.
+
+    Reference: https://youtu.be/cppD3NQJXb8
+    """
+
+    def test_get_homepage(self):
+        # Get the URL HTTP response code and template
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home/index.html')
--- pp4/settings.py
+++ pp4/settings.py
@@ -34,7 +34,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # CORS setting to allow Summernote editor
 X_FRAME_OPTIONS = 'SAMEORIGIN'
@@ -145,16 +145,18 @@ WSGI_APPLICATION = 'pp4.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+LOCAL_DB = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
-DATABASES = {
+PRODUCTION_DB = {
     'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
+
+DATABASES = LOCAL_DB
 
 
 # Password validation
