8eee27cbbaf39408ce87aab39e00d9bcbb58926e
Joseph <jts272@outlook.com>
2023-05-27 17:10:11 +0100

    test: services app string method

--- services/tests.py
+++ services/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from .models import Service
 
 # Create your tests here.
+
+
+class TestModels(TestCase):
+    def test_str_method(self):
+        """Coverage has shown that a test is required for the __str__
+        method of the service model.
+
+        Reference: https://youtu.be/o_CnAyqxyyo
+        """
+        # Create a service and give it a name (price is required)
+        service = Service.objects.create(name='Test Service', price=10)
+        self.assertEqual(service.__str__(), 'Test Service')
