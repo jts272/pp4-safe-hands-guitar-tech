79fd14da3a935af47fd5053af7ec297c4bc4dd29
Joseph <jts272@outlook.com>
2023-05-27 19:29:22 +0100

    test: post model string method

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from .models import Post
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your tests here.
+
+
+class TestPostModel(TestCase):
+    """This class will test the methods of the Post model."""
+
+    def test_str_method(self):
+        # Create an author, which a post must have
+        author = User(username='John')
+        author.save()
+        # Create a post with any required fields and a title to test
+        post = Post.objects.create(
+            created_on=timezone.now(), author=author, title='Test post')
+        self.assertEqual(post.__str__(), 'Test post')
--- services/tests.py
+++ services/tests.py
@@ -5,12 +5,13 @@ from .models import Service
 
 
 class TestModels(TestCase):
-    def test_str_method(self):
-        """Coverage has shown that a test is required for the __str__
-        method of the service model.
+    """Coverage has shown that a test is required for the __str__
+    method of the service model.
 
-        Reference: https://youtu.be/o_CnAyqxyyo
-        """
+    Reference: https://youtu.be/o_CnAyqxyyo
+    """
+
+    def test_str_method(self):
         # Create a service and give it a name (price is required)
         service = Service.objects.create(name='Test Service', price=10)
         self.assertEqual(service.__str__(), 'Test Service')
