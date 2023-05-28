20a4a6634685683679dde55846755135a6a5badf
Joseph <jts272@outlook.com>
2023-05-28 12:15:53 +0100

    build: deployment for validation testing

--- pp4/settings.py
+++ pp4/settings.py
@@ -34,7 +34,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # CORS setting to allow Summernote editor
 X_FRAME_OPTIONS = 'SAMEORIGIN'
@@ -156,7 +156,7 @@ PRODUCTION_DB = {
     'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
 
-DATABASES = LOCAL_DB
+DATABASES = PRODUCTION_DB
 
 
 # Password validation
